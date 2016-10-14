b51c9d6498b165a4ffab6b19f7a5f9fe3a1c33fc
Dan Falconer <github@crazedsanity.com>
2016-10-14 13:18:35 -0500

    Fix issue with tag message (now uses a temp file)

--- bin/_tagAndPush.sh
+++ bin/_tagAndPush.sh
@@ -9,15 +9,17 @@ v=`grep "VERSION:" VERSION | cut -f2 -d " "`
 p=`grep "PROJECT:" VERSION | cut -f2 -d " "`
 #TAG="git tag v$v"
 LASTTAG=`git describe --tags --abbrev=0`
+tmpfile=''
 EXTRALOGCMD=''
 if [ -n "$LASTTAG" ]; then
 	EXTRALOGCMD="${LASTTAG}..HEAD "
+	tmpfile=$(mktemp /tmp/_tagAndPush.XXXXX)
+	git log ${EXTRALOGCMD}--oneline > ${tmpfile}
 fi
-TAG="git tag v$v -a -m \"`git log ${EXTRALOGCMD}--oneline`\""
+TAG="git tag v${v} -a -F ${tmpfile}"
 PUSH="git push"
 PUSH2="git push --tags"
 CMD="$TAG && $PUSH && $PUSH2"
-#CMD="git tag v$v -a -m \"\`git log ${EXTRALOGCMD}--oneline\`\" && $PUSH && $PUSH2"
 
 echo "Tagging ${p} as ${v}... ANNOTATION: "
 echo "The following will be in the annotation: ";
@@ -27,3 +29,5 @@ echo -e "\t${CMD}"
 read -p "Press [enter] to continue..."
 
 $TAG && $PUSH && $PUSH2
+
+rm -f $tmpfile
